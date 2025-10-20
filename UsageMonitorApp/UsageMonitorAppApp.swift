e743c95472bcecb402696870df38360b444a20a0
Eric Gong <ericgong2005@gmail.com>
2025-10-20 14:40:25 -0400

    New UsageMonitor App

---
+++ UsageMonitorApp/UsageMonitorAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  UsageMonitorAppApp.swift
+//  UsageMonitorApp
+//
+//  Created by Eric Gong on 10/20/25.
+//
+
+import SwiftUI
+
+@main
+struct UsageMonitorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
